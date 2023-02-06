e8da397a49dfb8c05ebc4ceec54b437d79b9151b
Viktoryia Naskevich <vika8nask@gmail.com>
2023-02-06 22:29:32 +0300

    разные типы данных

---
+++ praktik-1.py
@@ -0,0 +1,12 @@
+name = "John Snow"
+age = 29
+
+print (name)
+
+num = 5
+str = 'hi'
+lst = [1, 5, 'pol']
+dct = {'name': 'vika', 'age': '22'}
+tup = (1,2,3,4,5)
+st = {'rec', 'ghgjg', '123'}
+print (bool (3))
