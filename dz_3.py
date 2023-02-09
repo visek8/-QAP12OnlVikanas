ead77395690c2a95f1a500937cc8b4a605380844
Viktoryia Naskevich <vika8nask@gmail.com>
2023-02-09 18:29:08 +0300

    add homework exercises number 1.3, 1.4, 1, 2, 3

--- dz_3.py
+++ dz_3.py
@@ -1,2 +1,36 @@
-print ("x = ", 17/2*3 + 2)
+# задание 1.3
+print("x = ", 17 / 2 * 3 + 2)
 
+print("x = ", 2 + 17 / 2 * 3)
+
+print("x = ", 19 % 4 + 15 / 2 * 3)
+
+print("x = ", (15 + 6) - 10 * 4)
+
+print("x = ", 17 / 2 % 2 * 3 ** 3)
+
+# задание 1.4
+age_1 = 23
+age_2 = 78
+age_3 = 33
+age_s = age_1 + age_2 + age_3
+print("Сумма возраста 3-х соседе равна: ", age_s)
+age_sa = age_s / 3
+print("Средний арифметический возраст равен: ", age_sa)
+
+# 1
+a = -1.6
+b = 2.99
+print(int(a))
+print(int(b))
+
+# 2
+txt = 'www.my_site.com#about'
+print(txt.replace("#", "/"))
+
+#3
+d = "stroka"
+c = "ing"
+print(d+c)
+
+#4
