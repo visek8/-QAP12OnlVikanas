245126ee7886d28ef16f2790a6fa6ccb6b5aea2c
Viktoryia Naskevich <vika8nask@gmail.com>
2023-03-10 00:07:34 +0300

    fixed functions for str file

---
+++ homework/dz_4/hdhdh.py
@@ -0,0 +1,43 @@
+# def hw():
+#   h_1: list
+#  h_1 = [1, 2, 3]
+#  h_2: list
+# h_2 = ['один', 'два', 'три']
+# print (h_1, h_2)
+# hw()
+
+
+# def hw():
+#   return hw_1
+
+
+# hw_1 = [1, 2, 3]
+# print(hw_1)
+
+#test_tring: str = 'Hello world!'
+
+
+#def kp(test_tring):
+ #   test_tring: str
+  #  test_tring = 'Hello world!'
+   # a = test_tring.find('w')
+    #b = test_tring.count('l')
+    #c = test_tring.startswith('Hello')
+    #d = test_tring.endswith('qwe')
+    #print(a, b, c, d, sep='\n')
+
+
+#kp(test_tring)
+
+
+# 3 Есть строка: “my name is name”. Напечатайте ее, но вместо 2ого “name” вставьте
+# ваше имя
+def p():
+    y: str = 'my name is name'
+    print(y.rfind('name'))
+    y = y[:11]
+    print(y + 'Viktoryia')
+
+p()
+
+
--- homework/dz_4/str.py
+++ homework/dz_4/str.py
@@ -1,46 +1,59 @@
-# 1
+# 1 Свяжите переменную с любой строкой, состоящей не менее чем из 8 символов.
+# Извлеките из строки первый символ, затем последний, третий с начала и третий с
+# конца. Измерьте длину вашей строки.
 def a():
-    return v
+    v: str = 'grfnklgbjksb'
+    print(len(v), v[0], v[-1], v[2], v[-3])
 
 
-v: str = 'grfnklgbjksb'
-print(len(v), v[0], v[-1], v[2], v[-3])
+a()
 
 
-# 2
+# 2 Присвойте произвольную строку длиной 10-15 символов переменной и извлеките из
+# нее следующие срезы:
+# ● первые восемь символов
+# ● четыре символа из центра строки
+# ● символы с индексами кратными трем
+# ● переверните строку
 def s():
-    return m
+    m: str = 'bhdjkbahjkcbaj'
+    print(m[0:8])
+    pol = len(m) / 2  # узнаем середину списка
+    n: float = pol - 2  # узнаем начало среза
+    k: float = pol + 2  # узнаем конец среза
+    print(n, k, m[5:9], m[2::3], m[::-1])
 
 
-m: str = 'bhdjkbahjkcbaj'
-print(m[0:8])
-pol = len(m) / 2  # узнаем середину списка
-n: float = pol - 2  # узнаем начало среза
-k: float = pol + 2  # узнаем конец среза
-print(n, k)  # выводим значения, которые необходимо будет ввести в срез
-print(m[5:9])  # выводим 4 символа из середины строки
-print(m[2::3])  # выводит элементы с индексами, кратными 3
-print(m[::-1])  # перевернули строку
+s()
 
 
-# 3
+# 3 Есть строка: “my name is name”. Напечатайте ее, но вместо 2ого “name” вставьте
+# ваше имя
 def p():
-    return y
+    y: str = 'my name is name'
+    print(y.rfind('name'))
+    y = y[:11]
+    print(y + 'Viktoryia')
 
 
-y: str = 'my name is name'
-print(y.rfind('name'))
-y = y[:11]
-print(y + 'Viktoryia')
+p()
 
-
-# 4
-def kp():
-    return test_tring
-
-
+# 4  Есть строка: test_tring = "Hello world!", необходимо
+# ● напечатать на каком месте находится буква w
+# ● кол-во букв l
+# ● Проверить начинается ли строка с подстроки: “Hello”
+# ● Проверить заканчивается ли строка подстрокой: “qwe”
 test_tring: str = 'Hello world!'
-print(test_tring.find('w'))
-print(test_tring.count('l'))
-print(test_tring.startswith('Hello'))
-print(test_tring.endswith('qwe'))
+
+
+def kp(test_tring):
+    test_tring: str
+    test_tring = 'Hello world!'
+    a = test_tring.find('w')
+    b = test_tring.count('l')
+    c = test_tring.startswith('Hello')
+    d = test_tring.endswith('qwe')
+    print(a, b, c, d, sep='\n')
+
+
+kp(test_tring)
