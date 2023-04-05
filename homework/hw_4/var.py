949d5bc0cdc444d380cda741b722d74dba73102f
Viktoryia Naskevich <vika8nask@gmail.com>
2023-04-06 00:16:45 +0300

    rename rep-ry

--- homework/hw_4/var.py
+++ homework/hw_4/var.py
@@ -6,8 +6,8 @@ big_int = var_int * 3.5
 
 var_float = var_float - 1
 
-var_int/var_float
-big_int/var_float
+var_int / var_float
+big_int / var_float
 
-var_str = var_str*2 + "Yes"*3
+var_str = var_str * 2 + "Yes" * 3
 print(var_float, var_int, var_str, big_int)
