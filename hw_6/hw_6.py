194cae9ffb341ac9c5fe15e03df143951fe75c7d
Viktoryia Naskevich <vika8nask@gmail.com>
2023-04-10 15:05:01 +0300

    add homework num 6, but second exercise work with error

--- hw_6/hw_6.py
+++ hw_6/hw_6.py
@@ -19,8 +19,50 @@ else:
     print('Error')
 f_2.close()
 
-#2 2 Дан файл целых чисел. Создать два новых файла, первый из которых
-#содержит четные числа из исходного файла, а второй — нечетные (в том
-#же порядке). Если четные или нечетные числа в исходном файле
-#отсутствуют, то соответствующий результирующий файл оставить
-#пустым.
+# 2 Дан файл целых чисел. Создать два новых файла, первый из которых
+# содержит четные числа из исходного файла, а второй — нечетные (в том
+# же порядке). Если четные или нечетные числа в исходном файле
+# отсутствуют, то соответствующий результирующий файл оставить
+# пустым.
+with open("text_1.txt", "r") as n:
+    file_2 = n.read()
+    arr = file_2.split(' ')
+    for i in arr:
+        if int(i) % 2 == 0:
+            f_3 = open('text_2.txt', 'w')
+            f_3.write(str(i))
+        else:
+            f_4 = open('text_3.txt', 'w')
+            f_4.write(str(i))
+f_4.close()
+f_3.close()
+
+# 3 Дан файл вещественных чисел. Заменить в нем все элементы на их
+# квадраты
+with open('text_1.txt') as f:
+    number_lines = f.readlines()
+
+with open('text_1.txt', 'w') as f:
+    for line in number_lines:
+        line = line.split(' ')
+        f.write(' '.join(map(lambda x: str(float(x) ** 2), line)))
+
+# 4 Даны два файла произвольного типа. Поменять местами их
+# содержимое. Файлы должны быть бинарного типа
+
+with open ('binfile_1.bin', 'wb') as b_1:
+    num_1 = [5, 10, 15, 20, 25, 30]
+    t_1 = bytearray(num_1)
+    b_1.write(t_1)
+with open ('binfile_2.bin', 'wb') as b_2:
+    num_2 = [7, 12, 17, 22, 27, 32]
+    t_2 = bytearray(num_2)
+    b_2.write(t_2)
+with open ('binfile_1.bin', 'rb') as bb_1:
+    bin_1 = bb_1.read()
+with open('binfile_2.bin', 'rb') as bb_2:
+    bin_2 = bb_2.read()
+with open ('binfile_1.bin', 'wb') as b_11:
+    b_11.write(bin_2)
+with open ('binfile_2.bin', 'wb') as b_22:
+    b_22.write(bin_1)
